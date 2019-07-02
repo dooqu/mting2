c39c4c90b73c01e960871ed3b5dfc5990a1d924e
liuhe_android <liuhe_android@sina.com>
2019-07-02 14:32:39 +0800

    增加详情页

--- app/src/main/java/cn/xylink/mting/ui/SplashActivity.java
+++ app/src/main/java/cn/xylink/mting/ui/SplashActivity.java
@@ -2,18 +2,15 @@ package cn.xylink.mting.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import cn.xylink.mting.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
