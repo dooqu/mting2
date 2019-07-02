3afc80de32ff0d1c789a28bd06db3b25fd296d0b
liuhe_android <liuhe_android@sina.com>
2019-07-02 10:41:08 +0800

    增加butterknife

--- app/src/main/java/cn/xylink/mting/ui/MainActivity.java
+++ app/src/main/java/cn/xylink/mting/ui/MainActivity.java
@@ -2,10 +2,14 @@ package cn.xylink.mting.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.xylink.mting.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
