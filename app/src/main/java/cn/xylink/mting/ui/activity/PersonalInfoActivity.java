897feb6144ec4325e224453478149dfcf77394eb
姚洪峰 <yaohongfeng@xylink.cn>
2019-07-24 16:09:31 +0800

    Splash  Permission

--- app/src/main/java/cn/xylink/mting/ui/activity/PersonalInfoActivity.java
+++ app/src/main/java/cn/xylink/mting/ui/activity/PersonalInfoActivity.java
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.jph.takephoto.app.TakePhoto;
